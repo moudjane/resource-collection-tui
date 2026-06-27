1e6ea2f1bd476ae41abd9b40b8ed92aea5ce1f8e
moudjane <mathis.oudjane@gmail.com>
2026-06-27 17:02:01 +0200

    style: format code with cargo fmt

--- src/simulation_tests.rs
+++ src/simulation_tests.rs
@@ -5,6 +5,13 @@ use super::*;
 #[test]
 fn prefers_fresh_positions_over_recent_ones() {
     let recent = VecDeque::from([Position { x: 1, y: 0 }, Position { x: 0, y: 1 }]);
-    let candidates = [Position { x: 1, y: 0 }, Position { x: 2, y: 0 }, Position { x: 0, y: 1 }];
-    assert_eq!(choose_non_repeating_position(&candidates, &recent), Some(Position { x: 2, y: 0 }));
+    let candidates = [
+        Position { x: 1, y: 0 },
+        Position { x: 2, y: 0 },
+        Position { x: 0, y: 1 },
+    ];
+    assert_eq!(
+        choose_non_repeating_position(&candidates, &recent),
+        Some(Position { x: 2, y: 0 })
+    );
 }
